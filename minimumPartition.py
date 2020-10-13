7019cd01ceb6979b127c00cd4bc8af186b19287a
Abhishek <abhishekt2024@gmail.com>
2020-10-13 15:49:51 +0530

    minimumPartition completed

--- minimumPartition.py
+++ minimumPartition.py
@@ -1,7 +1,9 @@
 def minPartition(a,n,s1,s2):
+    if n==1:
+        return min((abs((s1+a[0])-s2)),(abs(s1-(s2+a[0]))))
+    else:
+        return min(minPartition(a,n-1,s1+a[n-1],s2),minPartition(a,n-1,s1,s2+a[n-1]))
 
-
-
-arr = [1,2,11,4,1]
+arr = [5,1,6]
 s1=s2=0
-minPartition(arr,len(arr),s1,s2)
+print(minPartition(arr,len(arr),s1,s2))
